aed0d3e25d98aabc326acc5f292abea3e0c2eb01
Rana Sahin <sahinra222@gmail.com>
2022-05-03 11:26:27 +0200

    Add project files.

---
+++ Rasterization/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rasterization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void CanvasLeftUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CanvasLeftDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void LineButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CircleButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PolygonButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MoveButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
